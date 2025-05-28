5a0b40caf1ea93cb263891199aa6f8a3bfdd878b
Jonathan <jonyoner@gmail.com>
2025-05-28 10:09:49 +0000

    Añado boton para generar en consola los dominios

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ['the', 'our'];
+
+/* let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 
@@ -15,5 +16,20 @@ for (let pronouns of pronoun){
     }
   }
 }
+ */
 
+function generarDominios() {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
 
+  for (let pronouns of pronoun) {
+    for (let adjs of adj) {
+      for (let nouns of noun) {
+        console.log(`www.${pronouns}${adjs}${nouns}.com`);
+      }
+    }
+  }
+}
+
+window.generarDominios = generarDominios;
